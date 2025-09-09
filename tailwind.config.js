18b142a7e059e9fd2953ad13e4b66d511c6b8232
bynishtha <bynishtha@gmail.com>
2025-09-09 22:31:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // include all your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
